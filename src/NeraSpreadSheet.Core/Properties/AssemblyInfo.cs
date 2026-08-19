4c169c7ff306591651df588643bee7e48b72db2f
HoangHung997 <103053981+HoangHung997@users.noreply.github.com>
2026-08-19 15:07:43 +0700

    chore: allow OpenXml exact sparse style serialization

--- src/NeraSpreadSheet.Core/Properties/AssemblyInfo.cs
+++ src/NeraSpreadSheet.Core/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
 using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("NeraSpreadSheet.Editing")]
+[assembly: InternalsVisibleTo("NeraSpreadSheet.OpenXml")]
 [assembly: InternalsVisibleTo("NeraSpreadSheet.Core.Tests")]
