f04f2c99cedcdb2d3914851308fae2036e34f948
HoangHung997 <103053981+HoangHung997@users.noreply.github.com>
2026-08-21 17:31:41 +0700

    test: activate loaded MAUI Table filter smoke window deterministically

--- tests/NeraSpreadSheet.Maui.Windows.TableFilterSmoke/SmokeApplication.cs
+++ tests/NeraSpreadSheet.Maui.Windows.TableFilterSmoke/SmokeApplication.cs
@@ -1,16 +1,27 @@
 using Microsoft.Maui;
 using Microsoft.Maui.Controls;
+using WinUiWindow = Microsoft.UI.Xaml.Window;
 
 namespace NeraSpreadSheet.Maui.Windows.TableFilterSmoke;
 
 public sealed class SmokeApplication : Application
 {
     protected override Window CreateWindow(
-        IActivationState? activationState) =>
-        new(new SmokePage())
+        IActivationState? activationState)
+    {
+        var window = new Window(new SmokePage())
         {
             Title = "NeraSpreadSheet MAUI Table filter smoke",
             Width = 960d,
             Height = 640d,
         };
+        window.HandlerChanged += (_, _) =>
+        {
+            if (window.Handler?.PlatformView is WinUiWindow nativeWindow)
+            {
+                nativeWindow.Activate();
+            }
+        };
+        return window;
+    }
 }
