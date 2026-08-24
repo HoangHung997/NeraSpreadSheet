429eae78571b54b5c5db80dc7032a0ee4405deb3
HoangHung997 <103053981+HoangHung997@users.noreply.github.com>
2026-08-24 10:57:48 +0700

    feat: register advanced statistical functions

--- src/NeraSpreadSheet.Formulas/StandardFormulaFunctions.cs
+++ src/NeraSpreadSheet.Formulas/StandardFormulaFunctions.cs
@@ -26,6 +26,10 @@ internal static class StandardFormulaFunctions
         {
             yield return function;
         }
+        foreach (var function in AdvancedStatisticalFormulaFunctions.Create())
+        {
+            yield return function;
+        }
         foreach (var function in FinancialFormulaFunctionsHardened.Create())
         {
             yield return function;
