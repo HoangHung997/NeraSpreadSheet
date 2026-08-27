e8d395484da1c4d44c3c96e2aed1252c5e88e338
HoangHung997 <103053981+HoangHung997@users.noreply.github.com>
2026-08-28 00:53:03 +0700

    test(maui): add Mac Catalyst smoke entry point

---
+++ tests/NeraSpreadSheet.Maui.MacCatalyst.AnalyticsSmoke/Platforms/MacCatalyst/Program.cs
@@ -0,0 +1,9 @@
+using UIKit;
+
+namespace NeraSpreadSheet.Maui.MacCatalyst.AnalyticsSmoke;
+
+public static class Program
+{
+    public static void Main(string[] args) =>
+        UIApplication.Main(args, null, typeof(AppDelegate));
+}
