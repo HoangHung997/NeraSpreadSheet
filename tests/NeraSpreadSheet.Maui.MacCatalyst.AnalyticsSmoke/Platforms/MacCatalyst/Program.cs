4cb5c4b52f88b9f2abc41ff61b2aa3658b53be1f
HoangHung997 <103053981+HoangHung997@users.noreply.github.com>
2026-08-28 01:08:07 +0700

    test(maui): launch renamed Mac Catalyst app delegate

--- tests/NeraSpreadSheet.Maui.MacCatalyst.AnalyticsSmoke/Platforms/MacCatalyst/Program.cs
+++ tests/NeraSpreadSheet.Maui.MacCatalyst.AnalyticsSmoke/Platforms/MacCatalyst/Program.cs
@@ -5,5 +5,5 @@ namespace NeraSpreadSheet.Maui.MacCatalyst.AnalyticsSmoke;
 public static class Program
 {
     public static void Main(string[] args) =>
-        UIApplication.Main(args, null, typeof(AppDelegate));
+        UIApplication.Main(args, null, typeof(SmokeApplicationDelegate));
 }
