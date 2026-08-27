f570a41b9c10bfc650f0387519a05a22d6fac0c3
HoangHung997 <103053981+HoangHung997@users.noreply.github.com>
2026-08-27 15:29:41 +0700

    test(maui): expose analytics internals to loaded smoke

--- src/NeraSpreadSheet.Maui/Properties/AssemblyInfo.cs
+++ src/NeraSpreadSheet.Maui/Properties/AssemblyInfo.cs
@@ -2,3 +2,4 @@ using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("NeraSpreadSheet.Maui.Tests")]
 [assembly: InternalsVisibleTo("NeraSpreadSheet.Maui.Windows.Smoke")]
+[assembly: InternalsVisibleTo("NeraSpreadSheet.Maui.Windows.AnalyticsSmoke")]
