72f0de104315b3b9e51082c168dcf1190875880b
HoangHung997 <103053981+HoangHung997@users.noreply.github.com>
2026-08-28 10:07:54 +0700

    test: expose Mac Catalyst GPU diagnostics to smoke

--- src/NeraSpreadSheet.Maui/Properties/AssemblyInfo.cs
+++ src/NeraSpreadSheet.Maui/Properties/AssemblyInfo.cs
@@ -3,3 +3,4 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("NeraSpreadSheet.Maui.Tests")]
 [assembly: InternalsVisibleTo("NeraSpreadSheet.Maui.Windows.Smoke")]
 [assembly: InternalsVisibleTo("NeraSpreadSheet.Maui.Windows.AnalyticsSmoke")]
+[assembly: InternalsVisibleTo("NeraSpreadSheet.Maui.MacCatalyst.AnalyticsSmoke")]
